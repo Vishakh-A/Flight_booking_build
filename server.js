528c754fab61adbf61c6931abe2f5d27ccdd0068
Vishakh A <vishakhaarayalil@gmail.com>
2024-03-22 11:13:13 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,108 @@
+require('dotenv').config()
+const express =require('express');
+const app = express();
+const cors = require('cors');
+const axios = require('axios')
+const bodyParser = require('body-parser')
+const Port = process.env.PORT || 3000 ||4565;
+const path=require('path')
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+
+app.use(express.static('./dist'))
+app.post('/api',async (req,res)=>{
+
+  console.log("get");
+
+    const {flyTo,flyFrom,departingDate,travelClass,adultsNum,childNum}=req.body;
+    console.log(req.body);
+
+    const options = {
+      method: 'GET',
+      url: 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights',
+      params: {
+        sourceAirportCode: flyFrom,
+        destinationAirportCode: flyTo,
+        date: departingDate,
+        itineraryType: 'ONE_WAY',
+        sortOrder: 'PRICE',
+        numAdults: adultsNum,
+        numSeniors: childNum,
+        classOfService: travelClass,
+        pageNumber: '1',
+        currencyCode: 'INR'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.SRCH_FLIGHT_KEY,
+        'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
+      }
+    };
+
+    const flightFairOptions =  {
+      method: 'GET',
+      url: 'https://flight-fare-search.p.rapidapi.com/v2/flights/',
+      params: {
+        from: flyFrom,
+        to:flyTo,
+        date:departingDate,
+        adult: '1',
+        child:'2',
+        type: travelClass,
+        currency: 'INR'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.FLT_FARE_KEY,
+    'X-RapidAPI-Host': 'flight-fare-search.p.rapidapi.com'
+  
+      }
+
+      
+    };
+
+      const response = await axios.request(options);
+      const flightFareResponse = await axios.request(flightFairOptions);
+    
+
+      if(response.data.message ==='Success'){
+        
+
+        const filterFlightData = response.data.data.flights.map(item=>{
+           let dataObj={flightName:'',price:2121,depDate:'2023-12-6',arrivalDate:'2023-12-06',depCity:flyFrom,arrivalCity:flyTo}
+
+           dataObj.flightName = item.segments[0].legs[0].operatingCarrierCode.concat(item.segments[0].legs[0].flightNumber);
+           dataObj.price =item.purchaseLinks[0].totalPrice;
+           dataObj.depDate =  item.segments[0].legs[0].departureDateTime;
+           dataObj.arrivalDate = item.segments[0].legs[0].arrivalDateTime
+
+           if(flightFareResponse.data.results){
+           dataObj.depCity = flightFareResponse.data.results[0].departureAirport.city,
+           dataObj.arrivalCity = flightFareResponse.data.results[0].arrivalAirport.city
+           }
+          
+           return dataObj
+        })
+        console.log(filterFlightData);
+       
+        if(filterFlightData.length>0){
+            return res.json({message : 'success',data:filterFlightData});
+        }else{
+            res.json({message:'No Flights',data:[]});
+        }
+
+    }else{
+        res.json({message:'No Flights',data:[]});
+    }
+})
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'dist/index.html'))
+})
+app.listen(Port,()=>{
+
+    console.log(`Server @${Port}`);
+})
